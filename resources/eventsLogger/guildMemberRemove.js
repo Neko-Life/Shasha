7eb0f91e20effde749f20a5e7c2897ab1eef22b1
Neko Life <nekolife123579@gmail.com>
2021-06-28 17:14:50 +0900

    backtick

--- resources/eventsLogger/guildMemberRemove.js
+++ resources/eventsLogger/guildMemberRemove.js
@@ -14,15 +14,15 @@ module.exports = (member) => {
         const log = getChannel(member, member.guild.eventChannels.leave);
         if (!log) return;
         const days = Math.floor(new Date(new Date().valueOf() + member.client.matchTimestamp - member.joinedAt.valueOf()).valueOf() / 86400000),
-        emb = defaultEventLogEmbed(member.guild);
+            emb = defaultEventLogEmbed(member.guild);
         emb
-        .setTitle("Member `" + member.user.tag + "` left")
-        .setThumbnail(member.user.displayAvatarURL({format: "png", size: 4096, dynamic: true}))
-        .setColor(getColor("yellow"))
-        .addField("Registered", "**" + member.user.createdAt.toUTCString().slice(0, -4) + "**", true)
-        .addField("Joined", "**" + member.joinedAt.toUTCString().slice(0, -4) + "**" + `\n(${days > 0 ? `${days} day${days > 1 ? "s" : ""} ago` : "Today"})`, true)
-        .addField("Roles", member.roles.cache.size > 1 ? "<@&" + member.roles.cache.sort((a, b) => b.position - a.position).map(r => r.id).slice(0, -1).join(">, <@&") + ">" : "`[NONE]")
-        .setDescription(`<@!${member.id}> (${member.id}) just left.\nWe have ${member.guild.memberCount} total members now.`);
+            .setTitle("Member `" + member.user.tag + "` left")
+            .setThumbnail(member.user.displayAvatarURL({ format: "png", size: 4096, dynamic: true }))
+            .setColor(getColor("yellow"))
+            .addField("Registered", "**" + member.user.createdAt.toUTCString().slice(0, -4) + "**", true)
+            .addField("Joined", "**" + member.joinedAt.toUTCString().slice(0, -4) + "**" + `\n(${days > 0 ? `${days} day${days > 1 ? "s" : ""} ago` : "Today"})`, true)
+            .addField("Roles", member.roles.cache.size > 1 ? "<@&" + member.roles.cache.sort((a, b) => b.position - a.position).map(r => r.id).slice(0, -1).join(">, <@&") + ">" : "`[NONE]`")
+            .setDescription(`<@!${member.id}> (${member.id}) just left.\nWe have ${member.guild.memberCount} total members now.`);
         return trySend(member.client, log, emb);
     }
 }
