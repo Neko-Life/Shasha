ba0f08d193e92f59d4b53b9da4b3753eb6c9e7fe
Neko Life <nekolife123579@gmail.com>
2021-09-13 04:17:00 +0700

    Update nsfw.js

--- rsc/cmds/nsfw.js
+++ rsc/cmds/nsfw.js
@@ -3,22 +3,23 @@
 const { Command } = require("../classes/Command");
 const nsfwCmd = require("../rsc/nsfwCmd");
 
-const INTERACT_ENDPOINTS = ["ass", "athighs", "blow", "boobs", "feet", "furfuta", "furgif", "futa", "gifs", "hboobs", "hentai", "hfeet", "jackopose", "milk", "pantsu", "sex", "slime", "trap", "yuri"];
+const NSFW_ENDPOINTS = ["ass", "athighs", "blow", "boobs", "feet", "furfuta", "furgif", "futa", "gifs", "hboobs", "hentai", "hfeet", "jackopose", "milk", "pantsu", "sex", "slime", "trap", "yuri"];
 
-for (const IE of INTERACT_ENDPOINTS) {
-    module.exports[IE] = class extends Command {
-        constructor(interaction) {
-            super(interaction, {
-                name: "NSFW",
-                clientPermissions: ["EMBED_LINKS"]
-            });
-        }
 
-        async run(inter, { lewds, message }) {
-          if (!inter.channel.nsfw) return inter.reply("This is not an NSFW channel, baka!");
-            return nsfwCmd(inter, IE, lewds, message?.value);
-        }
+module.exports = class extends Command {
+    constructor(interaction) {
+        super(interaction, {
+            name: "nsfw",
+            clientPermissions: ["EMBED_LINKS"]
+        });
+    }
+
+    async run(inter, { category }) {
+        if (!inter.channel.nsfw) return inter.reply("This is not an NSFW channel, baka!");
+        if (!category) category = NSFW_ENDPOINTS[Math.floor(Math.random() * NSFW_ENDPOINTS.length)];
+        else category = category.value;
+        return nsfwCmd(inter, category);
     }
 }
 
-module.exports.constant = { INTERACT_ENDPOINTS }
+module.exports.constant = { NSFW_ENDPOINTS }
