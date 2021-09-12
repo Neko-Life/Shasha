30ed076390d3cfd141f5a8645809b2408349faaf
Night Raven <zkjcksn@gmail.com>
2021-09-12 10:50:01 -0400

    Add+Finish Lewds

--- rsc/cmds/nsfw.js
+++ rsc/cmds/nsfw.js
@@ -3,7 +3,7 @@
 const { Command } = require("../classes/Command");
 const nsfwCmd = require("../rsc/nsfwCmd");
 
-const INTERACT_ENDPOINTS = ["ass","athighs","blow","boobs","feet","furfuta","furgif","futa","gifs","hboobs","hentai","hfeet","jackopose","kink","milk","pantsu","sex","slime","thighs","trap","yuri"];
+const INTERACT_ENDPOINTS = ["ass", "athighs", "blow", "boobs", "feet", "furfuta", "furgif", "futa", "gifs", "hboobs", "hentai", "hfeet", "jackopose", "milk", "pantsu", "sex", "slime", "trap", "yuri"];
 
 for (const IE of INTERACT_ENDPOINTS) {
     module.exports[IE] = class extends Command {
@@ -15,8 +15,8 @@ for (const IE of INTERACT_ENDPOINTS) {
         }
 
         async run(inter, { lewds, message }) {
-          if (!inter.guild.channels.get(inter.channel.id).nsfw) return inter.reply("This is not an NSFW channel, baka!");
-            return interactCmd(inter, IE, lewds, message?.value);
+          if (!inter.channel.nsfw) return inter.reply("This is not an NSFW channel, baka!");
+            return nsfwCmd(inter, IE, lewds, message?.value);
         }
     }
 }
