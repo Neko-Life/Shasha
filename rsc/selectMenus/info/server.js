c5990b67b0b9dc31721beb2bd79a7a3a65e305dc
Neko-Life <nekolife123579@gmail.com>
2021-09-13 11:39:01 +0700

    other user support

--- rsc/selectMenus/info/server.js
+++ rsc/selectMenus/info/server.js
@@ -3,11 +3,23 @@
 const { isInteractionInvoker, replyFalseInvoker } = require("../../functions");
 
 async function handle(inter, args) {
-    if (!isInteractionInvoker(inter)) return replyFalseInvoker(inter, "/info server");
     const pages = inter.client.activeSelectMenus.get(inter.message.id);
     if (!pages) return inter.reply({ content: "This session's expired", ephemeral: true });
-    const ret = await inter.message.edit(pages[args[0]]);
-    await inter.deferUpdate();
+    if (!isInteractionInvoker(inter)) {
+        // return replyFalseInvoker(inter, "/info server");
+        const ePages = {};
+        for (const U in pages) {
+            const D = pages[U];
+            D.ephemeral = true;
+            ePages[U] = D;
+        }
+        await inter.reply(ePages[args[0]]);
+        inter.client.activeSelectMenus.set(inter.id, ePages);
+    }
+    if (!inter.replied) {
+        const ret = await inter.message.edit(pages[args[0]]);
+        await inter.deferUpdate();
+    } else await inter.editReply(pages[args[0]]);
 }
 
 module.exports = { handle }
