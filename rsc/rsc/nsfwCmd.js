ba6bab03917b8a47c52c6aaf3bc02f244c369918
Neko-Life <nekolife123579@gmail.com>
2021-09-13 11:38:42 +0700

    show picked category

--- rsc/rsc/nsfwCmd.js
+++ rsc/rsc/nsfwCmd.js
@@ -8,9 +8,9 @@ const getColor = require("../getColor");
 module.exports = async (interaction, query) => {
     const user = interaction.member || interaction.user;
     const URL = await lewds.nsfw(query);
-    if (!URL) return interaction.reply("Oopsie service is busy, guess you're gonna go dry...");
+    if (!URL) return interaction.reply("Oopsie our service is busy, guess you're gonna go dry...");
     const emb = new MessageEmbed()
-        .setAuthor((user.displayName || user.username),
+        .setAuthor((user.displayName || user.username) + "'s " + query + " 😳",
             (user.user || user).displayAvatarURL({
                 size: 128,
                 format: "png",
